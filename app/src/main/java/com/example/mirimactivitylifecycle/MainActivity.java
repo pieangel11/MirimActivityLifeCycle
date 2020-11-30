db05bbed9fe25f74ace6ff85670d5e11d7ca1d76
young <pieangel11@naver.com>
2020-11-30 20:13:02 +0900

    Complete ActivityLifeCtcle Test

---
+++ app/src/main/java/com/example/mirimactivitylifecycle/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.mirimactivitylifecycle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.i("액티비티 생명주기: ", "onCreate()");
+
+        Button btnCall = findViewById(R.id.btn_call);
+        Button btnFinish = findViewById(R.id.btn_finish);
+        btnCall.setOnClickListener(btnListener);
+        btnFinish.setOnClickListener(btnListener);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("액티비티 생명주기: ", "onResume()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("액티비티 생명주기: ", "onDestroy()");
+    }
+
+    View.OnClickListener btnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.btn_call:
+                    Uri uri = Uri.parse("tel:010-1111-3333");
+                    Intent intent = new Intent(Intent.ACTION_DIAL, uri);
+                    startActivity(intent);
+                    break;
+                case R.id.btn_finish:
+                    finish();
+            }
+        }
+    };
+}
